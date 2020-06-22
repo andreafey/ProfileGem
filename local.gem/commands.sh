102da437e427f6654b83306abc927c14eb5af60b
Andrea Fey <andrea.github@mailnull.com>
2020-06-22 14:00:20 -0400

    Add local.gem
    	new file:   local.gem/README.md
    	new file:   local.gem/aliases.sh
    	new file:   local.gem/base.conf.sh
    	new file:   local.gem/commands.sh
    	new file:   local.gem/environment.sh
    	new file:   local.gem/functions.sh
    	new file:   local.gem/info.txt

---
+++ local.gem/commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Commands to execute before ProfileGem finishes loading.
+#
+# Executed last, and only included in interactive terminals.
+#
+
+# unaBASHedly stolen/modfied
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
